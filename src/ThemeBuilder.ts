1a6df0b9fd1617f378bf3205c3b3b2f69c47592d
rhangai <hangai.renan@gmail.com>
2020-06-30 19:57:31 -0300

    chore: Updated copy of theme files

--- src/ThemeBuilder.ts
+++ src/ThemeBuilder.ts
@@ -1,6 +1,7 @@
 import { Compiler } from './compiler/Compiler';
 import fs from 'fs-extra';
 import path from 'path';
+import glob from 'fast-glob';
 
 export type ThemeBuilderOptions = {
 	themeDir: string;
@@ -10,6 +11,9 @@ export type ThemeBuilderOptions = {
 export class ThemeBuilder {
 	constructor(private readonly options: ThemeBuilderOptions) {}
 
+	private glob(patterns: string | string[]): Promise<string[]> {
+		return glob(patterns, { cwd: this.options.themeDir });
+	}
 	/**
 	 * Generate every file
 	 */
@@ -28,13 +32,30 @@ export class ThemeBuilder {
 	 * Build the theme
 	 */
 	async build(): Promise<Record<string, string | Buffer>> {
-		const output = await Promise.all([this.buildCompiler()]);
+		const output = await Promise.all([
+			this.buildCompiler(),
+			this.buildCopy(),
+		]);
 
 		let files = {};
 		for (const o of output) {
-			files = { ...o!.outputFiles };
+			files = { ...files, ...o!.outputFiles };
 		}
 		return files;
+	}
+
+	/**
+	 * Get the files to copy
+	 */
+	private async buildCopy() {
+		const inputFiles = await this.glob(['theme.properties', 'text/**/*']);
+		const outputFiles: Record<string, string | Buffer> = {};
+		for (const file of inputFiles) {
+			outputFiles[file] = await fs.readFile(
+				path.join(this.options.themeDir, file)
+			);
+		}
+		return { outputFiles };
 	}
 
 	/**
@@ -43,7 +64,7 @@ export class ThemeBuilder {
 	private async buildCompiler() {
 		const compiler = new Compiler({
 			themeDir: this.options.themeDir,
-			files: await this.getFiles(),
+			files: await this.getCompilerFiles(),
 		});
 		const { error, stats, outputFiles } = await compiler.build();
 		if (error) {
@@ -55,20 +76,18 @@ export class ThemeBuilder {
 	/**
 	 * Get the files to build
 	 */
-	private async getFiles() {
-		const extensions = ['.mjml.pug', '.mjml', '.pug', '.html'];
-		const inputFiles = await fs.readdir(this.options.themeDir);
+	private async getCompilerFiles() {
+		const inputFiles = await this.glob([
+			'*.{pug,mjml,html}',
+			'html/*.{pug,mjml,html}',
+		]);
 		const files: Record<string, string> = {};
 		for (const file of inputFiles) {
 			const basename = path.basename(file).toLowerCase();
-			for (const ext of extensions) {
-				if (basename.endsWith(ext)) {
-					const key = path.basename(basename, ext);
-					if (files[key]) throw new Error(`Duplicate file ${file}`);
-					files[key] = `./${file}`;
-					break;
-				}
-			}
+			if (basename.startsWith('_')) continue;
+			let key = file.split('.')[0];
+			if (files[key]) throw new Error(`Duplicate file ${file}`);
+			files[key] = `./${file}`;
 		}
 		return files;
 	}
